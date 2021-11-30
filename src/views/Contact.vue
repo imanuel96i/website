a3577390257cff9893f61e6ddf28a9db4b39558d
Manuel <imanuel96i@gmail.com>
2021-11-30 20:21:11 -0300

    Register new user via api axios

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -7,7 +7,7 @@
         <v-row align="end" class="lightbox white--text pa-2 fill-height">
           <v-col>
             <v-container>
-              <div class="headline">Contact Us</div>
+              <div class="headline">Registro</div>
             </v-container>
           </v-col>
         </v-row>
@@ -15,38 +15,87 @@
     </div>
     <div class="block">
       <v-container>
+        <v-alert type="success" v-if="reg==true">
+          Se ha registrado correctamente.
+        </v-alert>
+        <v-container v-if="reg==false">
+          <v-alert type="error" v-if="regerr.email!=null || regerr.email!=''">
+            Este email ya se encuentra en uso
+          </v-alert>
+          <v-alert type="error" v-if="regerr.mobile!=null || regerr.mobile!=''">
+            Este numero ya se encuentra en uso
+          </v-alert>
+        </v-container>
         <v-form
           ref="form"
           v-model="valid"
           lazy-validation
         >
-          <v-text-field
-            v-model="name"
-            :counter="10"
-            :rules="nameRules"
-            label="Name"
-            required
-          ></v-text-field>
 
           <v-text-field
+            id="email"
             v-model="email"
             :rules="emailRules"
             label="E-mail"
             required
           ></v-text-field>
 
-          <v-textarea
-            v-model="message"
-            :rules="messageRules"
-            label="Message"
+          <v-text-field
+            v-model="password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[passwordRules.required, passwordRules.min,]"
+            :type="show1 ? 'text' : 'password'"
+            name="password"
+            label="Contraseña"
+            hint="Al menos 8 caracteres"
+            counter
+            @click:append="show1 = !show1"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="passwordconfirm"
+            :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[(password === passwordconfirm) || 'Las contraseñas deben coincidir', passwordRules.required, passwordRules.min,]"
+            :type="show2 ? 'text' : 'password'"
+            name="passwordconfirm"
+            label="Confirmar Contraseña"
+            hint="Las contraseñas deben coincidir"
+            counter
+            @click:append="show2 = !show2"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="named"
+            :counter="10"
+            :rules="nameRules"
+            label="Nombre"
+            id="named"
+            name="named"
             required
-          ></v-textarea>
+          ></v-text-field>
+
+          <v-text-field
+            v-model="lastname"
+            :counter="10"
+            :rules="nameRules"
+            label="Apellido"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            ref="mobile"
+            v-model="mobile"
+            :rules="mobileRules"
+            label="Numero de telefono"
+            required
+            placeholder="99999999"
+          ></v-text-field>
 
           <v-btn
             :disabled="!valid"
             color="success"
             class="mr-4"
-            @click="validate"
+            @click="postRegister"
           >
             Submit
           </v-btn>
@@ -61,41 +110,81 @@
         </v-form>
       </v-container>
     </div>
-    <div class="googlemap">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3300.7751489611774!2d-70.65221138425949!3d-34.17766604258751!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x96635acb40667985%3A0x7c52c12f99bd80ec!2sTarapac%C3%A1%20435%2C%20Machal%C3%AD%2C%20O&#39;Higgins!5e0!3m2!1ses-419!2scl!4v1637713125192!5m2!1ses-419!2scl" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-    </div>
   </v-main>
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
     name: 'Contact',
 
     data: () => ({
+      reg: null,
+      regerr: {
+        email: null,
+        mobile: null
+      },
+      show1: false,
+      show2: false,
+      password: '',
+      passwordconfirm: '',
       valid: true,
-      name: '',
+      lastname: '',
+      named: '',
       nameRules: [
-        v => !!v || 'Name is required',
+        v => !!v || 'Se requiere un nombre',
         v => (v && v.length <= 10) || 'Name must be less than 10 characters',
       ],
       email: '',
       emailRules: [
-        v => !!v || 'E-mail is required',
+        v => !!v || 'Se requiere un E-mail',
         v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
       ],
-      message: '',
-      messageRules: [
-        v => !!v || 'Message is required',
-        v => (v && v.length >= 10) || 'Message must be more than 10 characters',
+      mobile: '',
+      mobileRules: [
+        v => !!v || 'Se requiere un numero',
+        v => (v && v.length >= 8) || 'El numero debe contener al menos 8 digitos',
+        v => !/\D/.test(v) || 'Solo se admiten digitos',
       ],
+      passwordRules: {
+          required: value => !!value || 'Es necesario que ingrese una contraseña',
+          min: v => v.length >= 8 || 'La contraseña debe tener al menos 8 caracteres',
+      },
     }),
 
     methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
       reset () {
         this.$refs.form.reset()
+      },
+      postRegister (){
+        this.regerr.email = ''
+        this.regerr.mobile = ''
+        this.reg = null
+        axios.post('http://127.0.0.1:8000/UserMN/register',{
+          first_name: this.named,
+          last_name: this.lastname,
+          email: this.email,
+          mobile: this.mobile,
+          password: this.password
+        })
+        .then((response) =>{
+          console.log(response.data)
+          this.reg = true
+        })
+        .catch((err) =>{
+          console.error(err.response.data)
+          this.reg = false
+          if(err.response.data['email']!=null){
+            this.regerr.email = 'Este email ya se encuentra en uso'
+            if(err.response.data['mobile']!=null && this.regerr!=null){
+              this.regerr.mobile = 'Este numero ya esta en uso'
+            }
+          }
+          if(err.response.data['mobile']!=null && this.regerr==null){
+            this.regerr.mobile = 'Este numero ya se encuentra en uso'
+          }
+        })
       },
     },
   }

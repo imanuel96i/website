2098a8df6a9a53afd2e6025becfec9d459269b0a
Manuel Vidal <imanuel96i@gmail.com>
2022-02-05 15:04:28 -0300

    Social Media

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,6 +8,9 @@
         tile
         class="text-center"
       >
+        <v-card-text class="pt-0">
+          Social Media
+        </v-card-text>
         <v-card-text>
           <v-btn
             v-for="icon in icons"
@@ -21,9 +24,7 @@
           </v-btn>
         </v-card-text>
 
-        <v-card-text class="pt-0">
-          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-        </v-card-text>
+        
 
         <v-divider></v-divider>
 
